d08fab53ca670af2182387b8c5495e959be562af
Ajoni <adam584@onet.pl>
2019-12-11 17:18:53 +0100

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Class1> Class1s { get; set; }
+
+    }
+}
---
+++ Entities/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Entities
+{
+    public class Class1
+    {
+        public int id { get; set; }
+    }
+}
